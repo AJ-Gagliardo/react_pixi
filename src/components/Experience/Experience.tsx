014f22cd8fab8e151ef9933c781f053b753b463f
Antonio Gagliardo <ajgagliardo92@gmail.com>
2025-03-18 22:29:52 -0700

    canva size, width and height and recalculating it

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -1,7 +1,27 @@
 import { Stage } from "@pixi/react";
+import { calculateCanvasSize } from "../../helpers/common";
+import { useCallback, useEffect, useState } from "react";
 
 const Experience = () => {
-  return <Stage width={500} height={500}></Stage>;
+  const [canvasSize, setCanvasSize] = useState(calculateCanvasSize);
+
+  const updateCanvasSize = useCallback(() => {
+    setCanvasSize(calculateCanvasSize());
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("resize", updateCanvasSize);
+    return () => window.removeEventListener("resize", updateCanvasSize);
+  }, [updateCanvasSize]);
+
+  //this is also in common.ts
+  // export const calculateCanvasSize = () => {
+  //   const width = window.innerWidth;
+  //   const height = window.innerHeight;
+  //   return { width, height };
+  // };
+
+  return <Stage width={canvasSize.width} height={canvasSize.height}></Stage>;
 };
 
 export default Experience;
